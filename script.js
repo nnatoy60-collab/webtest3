8adf2b15017f869d995d6a4d8e88a77088de9909
nnatoy60-collab <nnatoy60@gmail.com>
2025-11-20 07:08:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Mobile Navbar toggle
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior:'smooth'
+    });
+    if(navLinks.classList.contains('active')) navLinks.classList.remove('active');
+  });
+});
